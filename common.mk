276714f894969b91014f631d2d36f08c41f0c284
jumoog <kilianvonpflugk@gmail.com>
2014-08-31 22:02:34 +0200

    use own blobs
    Change-Id: I6390945fb3992d6937d3029c818e87545a5afb3a

--- common.mk
+++ common.mk
@@ -170,4 +170,4 @@ TARGET_OMX_PATH := hardware/samsung/exynos/multimedia/openmax
 $(call inherit-product, hardware/samsung/exynos4x12.mk)
 
 # Include non-opensource parts
-$(call inherit-product, vendor/samsung/smdk4412-common/common-vendor.mk)
+$(call inherit-product, vendor/samsung_own/smdk4412-common/common-vendor.mk)
