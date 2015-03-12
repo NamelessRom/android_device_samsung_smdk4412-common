fd943da0c2a249a91cb4ddb71001cae9ebd5cc92
Dheeraj CVR <cvr.dheeraj@gmail.com>
2015-03-12 08:13:40 +0530

    smdk4412-common: fix wifi p2p
    Change-Id: Ie30f13e4aba6fe2d9b1ccc82f44aa310f47fc0a9

--- common.mk
+++ common.mk
@@ -48,7 +48,9 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    $(COMMON_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(COMMON_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(COMMON_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 PRODUCT_PACKAGES := \
     hostapd \
